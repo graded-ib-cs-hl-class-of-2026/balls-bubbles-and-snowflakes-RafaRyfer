b719c8a1eacb73fb0775a2b94f76ea81b39e5c66
Rafa <No>
2024-10-28 09:31:52 -0300

    proficient

--- src/Sketch.java
+++ src/Sketch.java
@@ -2,8 +2,20 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
-    /** Represents one ball */
     private Ball ball1;
+    private Ball ball2;
+    private Ball ball3;
+    private Ball ball4;
+
+    private Bubble bubble1;
+    private Bubble bubble2;
+    private Bubble bubble3;
+    private Bubble bubble4;
+
+    private Snowflake snowflake1;
+    private Snowflake snowflake2;
+    private Snowflake snowflake3;
+    private Snowflake snowflake4;
 
     /**
      * This method can only be used to change the window size. It runs before the
@@ -19,6 +31,24 @@ public class Sketch extends PApplet {
      */
     public void setup() {
         ball1 = new Ball(this);
+        ball2 = new Ball(this, 12, 100, 100, 4, -2);
+        ball3 = new Ball(this, 26, 300, 300, -1, -13);
+        ball4 = new Ball(this, 10, 25, 400, -5, 2);
+
+        bubble1 = new Bubble(this);
+        bubble2 = new Bubble(this, 40, 98, 200, 2, -0.5f);
+        bubble3 = new Bubble(this, 8, 50, 150, 1, -0.5f);
+        bubble4 = new Bubble(this, 22, 200, 300, 1.5f, -0.5f);
+
+        snowflake1 = new Snowflake(this);
+        snowflake2 = new Snowflake(this, 22, 134, 231, 1.5f, -0.5f);
+        snowflake3 = new Snowflake(this, 17, 50, 90, 1.5f, -0.5f);
+        snowflake4 = new Snowflake(this, 30, 200, 76, 1.5f, -0.5f);
+
+        // Mr. Griswold helped me understand color
+        ball2.setColors(color(78, 200, 100), 0);
+        ball3.setColors(color(64, 17, 178), 0);
+        ball4.setColors(color(222, 97, 255), 0);
     }
 
     /**
@@ -30,6 +60,31 @@ public class Sketch extends PApplet {
         background(180, 180, 255);
         ball1.draw();
         ball1.move();
+        ball2.draw();
+        ball2.move();
+        ball3.move();
+        ball3.draw();
+        ball4.move();
+        ball4.draw();
+
+        bubble1.draw();
+        bubble1.move();
+        bubble2.draw();
+        bubble2.move();
+        bubble3.draw();
+        bubble3.move();
+        bubble4.draw();
+        bubble4.move();
+
+        snowflake1.draw();
+        snowflake1.move();
+        snowflake2.draw();
+        snowflake2.move();
+        snowflake3.draw();
+        snowflake3.move();
+        snowflake4.draw();
+        snowflake4.move();
+      
     }
 
     /** All processing sketches have to use this main method. Don't touch this! */
--- src/Snowflake.java
+++ src/Snowflake.java
@@ -1,16 +1,81 @@
-/** THE ENTIRE CLASS IS INSIDE A COMMENT RIGHT NOW. TO ENABLE THE CLASS, 
- * DELETE THESE LINE AND THE LAST ONE IN THE FILE.
-
 class Snowflake {
-    // ONLY THE DRAW METHOD IS INCLUDED. YOU MUST FINISH THE REST.
-  
 
-    public void draw() {
-        line(x+radius,y,x-radius,y);
-        line(x,y+radius,x,y-radius);
-        line(x+radius*.707,y+radius*.707,x-radius*.707,y-radius*.707);
-        line(x+radius*.707,y-radius*.707,x-radius*.707,y+radius*.707);
-    }
+    /** The sketch that the snowflake is inside */
+    private Sketch s;
+    /** the radius of the snowflake */
+    private float radius;
+    private float x;
+    private float y;
+    /** The number of pixels the snowflake moves right per frame */
+    private float xSpeed;
+    /** The number of pixels the snowflake moves down per frame */
+    private float ySpeed;
+
+/** Empty constructor to keep the defaults. Only sets up our link. */
+public Snowflake(Sketch sketch) {
+    s = sketch;
+    radius = 20;
+    x = 100;
+    y = 100;
+    xSpeed = 2;
+    ySpeed = 1;
 }
 
-**/
+/** Fully specified constructor to allow changes to size, position, speed */
+/** Does NOT allow changing color! Need to use setColors() for that. */
+public Snowflake(Sketch sketch, float radius, float x, float y, float xspeed, float yspeed) {
+    this.s = sketch;
+    this.radius = radius;
+    this.x = x;
+    this.y = y;
+    this.xSpeed = xspeed;
+    this.ySpeed = -yspeed; //set y speed to negative cus its falling
+}
+
+// Accessors (getters) go here
+
+public float getRadius() {
+    return radius;
+}
+
+public float getDiameter() {
+    return radius * 2;
+}
+
+public float getX() {
+    return x;
+}
+
+public float getY() {
+    return y;
+}
+
+// Setters that you need go here - by default, only colors
+
+/**
+ * Draws the ball on the given sketch
+ */
+    public void draw() {
+        s.line(x+radius,y,x-radius,y);
+        s.line(x,y+radius,x,y-radius);
+        s.line(x+radius*.707f,y+radius*.707f,x-radius*.707f,y-radius*.707f);
+        s.line(x+radius*.707f,y-radius*.707f,x-radius*.707f,y+radius*.707f);
+    }
+
+    //copy and pasted from bubbles and it worked
+    /** */
+    public void move() {
+        x = x + xSpeed;
+        y = y + ySpeed;
+        if (x > s.width - radius || x < radius) {
+            xSpeed = -xSpeed; //just copied and pasted it from the ball code to have it bounce off the sides
+        } else if (y > s.height - radius) {
+            y = radius;
+        } else if (y < radius) {
+            y = s.height - radius;
+        }
+    }
+
+}
+
+
